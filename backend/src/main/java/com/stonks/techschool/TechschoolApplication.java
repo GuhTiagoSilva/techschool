c52f0ad6534d8116af748a7bdb38d43f87dd553a
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-06-02 19:56:02 -0300

    Initial Commit

---
+++ backend/src/main/java/com/stonks/techschool/TechschoolApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.techschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechschoolApplication.class, args);
+	}
+
+}
