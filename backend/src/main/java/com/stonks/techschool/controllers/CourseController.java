ab33e0f07579d77006bf869631b6b34099540a37
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-06-03 13:22:30 -0300

    Course Controller

---
+++ backend/src/main/java/com/stonks/techschool/controllers/CourseController.java
@@ -0,0 +1,68 @@
+package com.stonks.techschool.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.stonks.techschool.dto.CourseDTO;
+import com.stonks.techschool.services.CourseService;
+
+@RestController
+@RequestMapping(value = "/courses")
+public class CourseController {
+
+	@Autowired
+	private CourseService service;
+	
+	@PostMapping
+	public ResponseEntity<CourseDTO> insert(@RequestBody CourseDTO dto){
+		CourseDTO courseDTO = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(courseDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(courseDTO);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CourseDTO> update(@RequestBody CourseDTO dto, @PathVariable Long id){
+		CourseDTO courseDTO = service.update(id, dto);
+		return ResponseEntity.ok().body(courseDTO);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CourseDTO> findById(@PathVariable Long id){
+		CourseDTO courseDTO = service.findById(id);
+		return ResponseEntity.ok().body(courseDTO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<CourseDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<CourseDTO> result = service.findAllPaged(pageRequest);
+
+		return ResponseEntity.ok(result);
+	}
+}
